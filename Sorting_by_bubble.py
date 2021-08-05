cfd6cf5e79efd7d0a40735f1edb99e4bdb001b98
Krotov-git <krotov.ne@gmail.com>
2021-08-05 09:40:37 +0300

    Потому что есть мозго:?ы

--- Sorting_by_bubble.py
+++ Sorting_by_bubble.py
@@ -1,17 +1,15 @@
-def bubble_sort(arr):
-    '''
-    Функция сортирует массив от наименьшего к наибольшему
-    :param arr - список из элементов который нужно отсортировать
-    '''
+def sorting_by_bubble(spisok):
+#Функция сортирует список (он же массив) от меньшего к большему
+# в параметрах функции spisok - это список который нужно отсортировать
 
-    arr = arr.copy()
-    L = len(arr)
+    spisok = spisok.copy()
+    l = len(spisok)
 
-    for i in range(L - 1):
-        for j in range(L - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return arr
+    for i in range(l - 1):
+        for j in range(l - i - 1):
+            if spisok[j] > spisok[j + 1]:
+                spisok[j], spisok[j + 1] = spisok[j + 1], spisok[j]
+    return spisok
 
 s = [8, 2, 1, 3, 5, 4, 6, 7]
-print(bubble_sort(s))
+print(sorting_by_bubble(s))
